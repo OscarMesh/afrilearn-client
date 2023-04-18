ec5e71d970b91f9a179bd478f211b139d9efba7b
osacrMesh <okerekeifeanyi65@gmail.com>
2023-04-18 10:42:41 +0100

    search params

--- pages/subjects/index.tsx
+++ pages/subjects/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Subject } from "../../types";
 import axios from "axios";
 import SubjectCard from "../../components/SubjectCard";
@@ -8,17 +8,24 @@ interface IProps {
   subjects: Subject[];
 }
 
-const index = ({ subjects }: IProps) => {
+const index = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [allSubjects, setSubjects] = useState(subjects);
+  const [subjects, setSubjects] = useState<Subject[]>([]);
 
-  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const url = `http://localhost:5000/api/v1/subjects${
-      searchQuery ? `?q=${searchQuery}` : ""
-    }`;
-    const { data } = await axios.get(url);
-    setSubjects(data.subjects);
+  useEffect(() => {
+    const fetchSubjects = async () => {
+      const { data } = await axios.get(
+        `http://localhost:5000/api/v1/subjects${
+          searchQuery ? `?name=${searchQuery}` : ""
+        }`
+      );
+      setSubjects(data.subjects);
+    };
+    fetchSubjects();
+  }, [searchQuery]);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
   };
 
   return (
@@ -27,43 +34,26 @@ const index = ({ subjects }: IProps) => {
         <h3 className="font-bold text-[#29465b] text-[20px]">
           Find your perfect subjects and on-the-go
         </h3>
-        <form
-          onSubmit={handleSearch}
-          className="flex flex-row max-w-[400px] w-full  p-3 items-center justify-between rounded-lg  border "
-        >
+        <div className="flex flex-row max-w-[400px] w-full  p-3 items-center justify-between rounded-lg  border ">
           <input
             type="text"
             placeholder="Search Subjects..."
             className="outline-none border-none text-[#29465b]"
             value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
+            onChange={handleSearch}
           />
           <button type="submit">
             <CiSearch color="#29465b" size={30} />
           </button>
-        </form>
+        </div>
       </div>
       <div className="flex flex-col gap-10 videos h-full">
-        {allSubjects.length &&
-          allSubjects.map((subject: Subject) => (
-            <SubjectCard subject={subject} key={subject._id} />
-          ))}
+        {subjects.map((subject: Subject) => (
+          <SubjectCard subject={subject} key={subject._id} />
+        ))}
       </div>
     </div>
   );
 };
 
-export const getServerSideProps = async (context: any) => {
-  const { q } = context.query;
-  const searchQuery = q ? `?q=${q}` : "";
-  const { data } = await axios.get(
-    `http://localhost:5000/api/v1/subjects${searchQuery}`
-  );
-  const subjects = data.subjects;
-  return {
-    props: {
-      subjects: subjects,
-    },
-  };
-};
 export default index;
